5517d1317a0fe9b4a88b174fd6e05ed7ca405aed
João Silva <joao.silva5@decskill.com>
2025-09-10 09:01:09 +0100

    Add project files.

---
+++ Extension2/Command1.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using Microsoft;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell;
+using Microsoft.VisualStudio.Services.Identity;
+
+namespace Extension2
+{
+    /// <summary>
+    /// Command1 handler.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class Command1 : Command
+    {
+        private readonly TraceSource logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command1"/> class.
+        /// </summary>
+        /// <param name="traceSource">Trace source instance to utilize.</param>
+        public Command1(TraceSource traceSource)
+        {
+            // This optional TraceSource can be used for logging in the command. You can use dependency injection to access
+            // other services here as well.
+            this.logger = Requires.NotNull(traceSource, nameof(traceSource));
+        }
+
+        /// <inheritdoc />
+        public override CommandConfiguration CommandConfiguration => new("%Extension2.Command1.DisplayName%")
+        {
+            // Use this object initializer to set optional parameters for the command. The required parameter,
+            // displayName, is set above. DisplayName is localized and references an entry in .vsextension\string-resources.json.
+            Icon = new(ImageMoniker.KnownValues.Extension, IconSettings.IconAndText),
+            Placements = [CommandPlacement.KnownPlacements.ExtensionsMenu],
+        };
+
+        /// <inheritdoc />
+        public override Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            // Use InitializeAsync for any one-time setup or initialization.
+            return base.InitializeAsync(cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var desc = "Microsoft.IdentityModel.Claims.ClaimsIdentity;10267e99-ec0a-4d57-967d-5bf51a6bb9d6\\e123456@acbde.net";
+                var descriptor = TypeDescriptor.GetConverter(typeof(IdentityDescriptor));
+                var claims = descriptor.ConvertFrom(desc);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            await this.Extensibility.Shell().ShowPromptAsync("Hello from an extension!", PromptOptions.OK, cancellationToken);
+        }
+    }
+}
